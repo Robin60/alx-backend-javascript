d9f23cac7900504c85e3bc32614f9a3edd3065f0
Robin60 <owinorobin05@gmaaail.com>
2023-08-01 13:37:24 +0000

    feat: Javascript Data manipulation

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,10 @@
+/**
+ * GetListStudents that returns an array of objects.
+ */
+
+export default function getListStudents() {
+	return [
+	  { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+	  { id: 2, firstName: 'James', location: 'Columbia' },
+	  { id: 2, firstName: 'James', location: 'Columbia' }
+	  }
